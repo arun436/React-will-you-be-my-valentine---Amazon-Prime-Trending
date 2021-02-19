82069ae04ed223ecb858639a29638b01780843ca
arun436 <arunkumarchintha136@gmail.com>
2021-02-19 19:27:53 +0530

    Added disabling buttons

--- src/components/App.js
+++ src/components/App.js
@@ -21,9 +21,12 @@ const App = (props) => {
     <>
     <h1 data-testid="title">{props.slides[selectedState].title}</h1>    
     <p data-testid="text">{props.slides[selectedState].text}</p>
-    {selectedState < props.slides.length-1 ? <button data-testid="button-next" onClick={handleNextState}>Next</button> : null}
-    {props.slides.length - selectedState === 5 ? null : <button data-testid="button-prev" onClick={handlePrevState}>Previous</button>}
-    {props.slides.length - selectedState !== 5 ? <button data-testid="button-restart" onClick={handleRestartState}>Restart</button> : null}
+    {selectedState < props.slides.length-1 ? <button data-testid="button-next" onClick={handleNextState}>Next</button> :
+     <button data-testid="button-next" onClick={handleNextState} disabled>Next</button>}
+    {props.slides.length - selectedState === 5 ? <button data-testid="button-prev" onClick={handlePrevState} disabled>Previous</button> : 
+    <button data-testid="button-prev" onClick={handlePrevState}>Previous</button>}
+    {props.slides.length - selectedState !== 5 ? <button data-testid="button-restart" onClick={handleRestartState}>Restart</button> : 
+    <button data-testid="button-restart" onClick={handleRestartState} disabled>Restart</button>}
     </>
   )
 }
