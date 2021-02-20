7f77028418b76bd77b7bae851c63eb85056422e8
arun436 <arunkumarchintha136@gmail.com>
2021-02-20 13:21:18 +0530

    Added styling

--- src/components/App.js
+++ src/components/App.js
@@ -19,14 +19,18 @@ const App = (props) => {
 
   return (
     <>
+    <div id="main-container-text">
     <h1 data-testid="title">{props.slides[selectedState].title}</h1>    
     <p data-testid="text">{props.slides[selectedState].text}</p>
-    {selectedState < props.slides.length-1 ? <button data-testid="button-next" onClick={handleNextState}>Next</button> :
-     <button data-testid="button-next" onClick={handleNextState} disabled>Next</button>}
-    {props.slides.length - selectedState === 5 ? <button data-testid="button-prev" onClick={handlePrevState} disabled>Previous</button> : 
-    <button data-testid="button-prev" onClick={handlePrevState}>Previous</button>}
-    {props.slides.length - selectedState !== 5 ? <button data-testid="button-restart" onClick={handleRestartState}>Restart</button> : 
-    <button data-testid="button-restart" onClick={handleRestartState} disabled>Restart</button>}
+    </div>
+    <div id="main-container-buttons">
+    {selectedState < props.slides.length-1 ? <button data-testid="button-next" id="button-next"onClick={handleNextState}>Next</button> :
+     <button data-testid="button-next" id="button-next" onClick={handleNextState} disabled>Next</button>}
+    {props.slides.length - selectedState === 5 ? <button data-testid="button-prev" id="button-prev" onClick={handlePrevState} disabled>Previous</button> : 
+    <button data-testid="button-prev" id="button-prev" onClick={handlePrevState}>Previous</button>}
+    {props.slides.length - selectedState !== 5 ? <button data-testid="button-restart" id="button-restart" onClick={handleRestartState}>Restart</button> : 
+    <button data-testid="button-restart" id="button-restart"onClick={handleRestartState} disabled>Restart</button>}
+    </div>
     </>
   )
 }
